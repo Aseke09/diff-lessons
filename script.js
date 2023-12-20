12169e6b464354255a7f9aff7c0ba8e9fba2fccf
Askhat Tassybayev <a.tassybayev@gmail.com>
2023-12-20 23:18:28 +0600

    дополнены варианты с switch и многомерный массив

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let lang = 'ru'
+let lang = 'en'
 
 if(lang === 'ru'){
     console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье')
@@ -6,6 +6,24 @@ if(lang === 'ru'){
     console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 }
 
+
+switch(lang) {
+    case 'ru': 
+       console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье')
+       break;
+    case 'en':
+       console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+       break;
+    default:
+        console.log('Что-то произошло не так')       
+}
+
+let arr = []
+
+arr.ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+arr.en = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+console.log(arr[lang])
 // У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, 
 // если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
 //  Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
